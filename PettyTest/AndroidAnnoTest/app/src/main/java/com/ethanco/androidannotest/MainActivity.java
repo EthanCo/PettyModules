464d81d452b4a5cfc399ca35ff1d9a62f09586ee
EthanCo_DELL <fenghenyu@163.com>
2016-10-31 21:47:43 +0800

    add md

--- PettyTest/AndroidAnnoTest/app/src/main/java/com/ethanco/androidannotest/MainActivity.java
+++ PettyTest/AndroidAnnoTest/app/src/main/java/com/ethanco/androidannotest/MainActivity.java
@@ -1,10 +1,12 @@
 package com.ethanco.androidannotest;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.annotation.CallSuper;
 import android.support.annotation.CheckResult;
 import android.support.annotation.FloatRange;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresPermission;
 import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
@@ -23,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
 
         A a = new A();
         B b = new B();
-
     }
 
     public void setItemId(@NonNull String itemId) {
@@ -40,28 +41,29 @@ public class MainActivity extends AppCompatActivity {
 
     @CheckResult(suggest = "#check()")
     public String check(String s) {
-        s= s+"";
+        s = s + "";
         s = check();
         return s;
     }
 
+    //普通情况
     public String check() {
         Toast.makeText(getApplication(), "check", Toast.LENGTH_SHORT).show();
         return "check...";
     }
 
-    class A{
-        @CallSuper
-        public void say(){
+    class A {
+        @CallSuper //要求重写的幻术需要调用被重写的函数
+        public void say() {
             System.out.println("say");
         }
 
-        public void run(){
+        public void run() {
             System.out.println("run");
         }
     }
 
-    class B extends A{
+    class B extends A {
         @Override
         public void say() {
             super.say(); //注释掉这句会报错
@@ -71,5 +73,26 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
             super.run();
         }
+    }
+
+    //函数调用需要一个函数
+    @RequiresPermission(Manifest.permission.SET_WALLPAPER)
+    public void method1() {
+
+    }
+
+    //函数调用需要声明集合中最少一个权限
+    @RequiresPermission(anyOf = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION})
+    public void method2() {
+    }
+
+    //函数调用需要声明集合中最少一个权限
+    @RequiresPermission(anyOf = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION})
+    public void method3() {
+
     }
 }

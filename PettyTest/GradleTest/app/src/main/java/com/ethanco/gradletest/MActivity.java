cedfd790c6db096d9c22683391b57004b21500cc
EthanCo_DELL <fenghenyu@163.com>
2016-10-31 20:51:36 +0800

    Annotations

---
+++ PettyTest/GradleTest/app/src/main/java/com/ethanco/gradletest/MActivity.java
@@ -0,0 +1,17 @@
+package com.ethanco.gradletest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Zhk on 2016/10/20.
+ */
+
+public class MActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_);
+    }
+}

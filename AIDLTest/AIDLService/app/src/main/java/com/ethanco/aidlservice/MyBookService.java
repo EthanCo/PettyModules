03c557575d6b747a248b64aa51c50014b96d1bf0
EthanCo_DELL <fenghenyu@163.com>
2016-12-12 23:49:32 +0800

    增加注释

--- AIDLTest/AIDLService/app/src/main/java/com/ethanco/aidlservice/MyBookService.java
+++ AIDLTest/AIDLService/app/src/main/java/com/ethanco/aidlservice/MyBookService.java
@@ -9,17 +9,23 @@ import android.util.Log;
 
 import com.ethanco.aidlservice.bean.Book;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import static android.content.ContentValues.TAG;
 
 public class MyBookService extends Service {
 
-    private List<Book> books = new ArrayList<>();
+    //CopyOnWriteArrayList支持并发读/写    (类似的还有ConcurrentHashMap)
+    //AIDL方法是在服务端的Binder线程池中执行的，因此当多个客户端同时连接时，会存在多个线程同时访问的情形，所以我们要在AIDL方法中处理线程同步，而我们这里直接使用CopyOnWriteArrayList来进行自动的线程同步
+    private CopyOnWriteArrayList<Book> books = new CopyOnWriteArrayList<>();
+    //private List<Book> books = new ArrayList<>();
+
+
     //RemoteCallbackList 系统专门提供的用于删除跨进程listener的接口。(普通的对象、接口通过AIDL获取的不是同一个对象)，故不能进行unregisterListener，而RemoteCallbackList可以
     //1.RemoteCallbackList根据AIDL底层的Binder对象是同一个的特性，遍历服务端所有的listener，从而找到具有相同Binder对象的服务端listener
     //2.当客户端进程终止后，它能够自动移除客户端所注册的listener
+    //3.RemoteCallbackList内部自动实现了线程同步的功能，所以我们使用它来注册和解除注册的时候，不需要做额外的线程同步工作。
     private RemoteCallbackList<IOnNewBookArrivedListener> onNewBookArrivedListeners = new RemoteCallbackList<>();
     //private List<IOnNewBookArrivedListener> onNewBookArrivedListeners = new ArrayList<>();
     private BookManagerImpl bookManager = new BookManagerImpl();

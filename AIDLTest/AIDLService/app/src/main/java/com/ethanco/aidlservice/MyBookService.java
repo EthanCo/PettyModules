6206ba08a2ba99413716e184f0ba729f2ddf624d
EthanCo_DELL <fenghenyu@163.com>
2016-12-12 23:43:04 +0800

    使用RemoteCallbackList

--- AIDLTest/AIDLService/app/src/main/java/com/ethanco/aidlservice/MyBookService.java
+++ AIDLTest/AIDLService/app/src/main/java/com/ethanco/aidlservice/MyBookService.java
@@ -3,18 +3,25 @@ package com.ethanco.aidlservice;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.RemoteCallbackList;
 import android.os.RemoteException;
-import android.os.SystemClock;
+import android.util.Log;
 
 import com.ethanco.aidlservice.bean.Book;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.content.ContentValues.TAG;
+
 public class MyBookService extends Service {
 
     private List<Book> books = new ArrayList<>();
-    private List<IOnNewBookArrivedListener> onNewBookArrivedListeners = new ArrayList<>();
+    //RemoteCallbackList 系统专门提供的用于删除跨进程listener的接口。(普通的对象、接口通过AIDL获取的不是同一个对象)，故不能进行unregisterListener，而RemoteCallbackList可以
+    //1.RemoteCallbackList根据AIDL底层的Binder对象是同一个的特性，遍历服务端所有的listener，从而找到具有相同Binder对象的服务端listener
+    //2.当客户端进程终止后，它能够自动移除客户端所注册的listener
+    private RemoteCallbackList<IOnNewBookArrivedListener> onNewBookArrivedListeners = new RemoteCallbackList<>();
+    //private List<IOnNewBookArrivedListener> onNewBookArrivedListeners = new ArrayList<>();
     private BookManagerImpl bookManager = new BookManagerImpl();
 
     @Override
@@ -41,26 +48,37 @@ public class MyBookService extends Service {
         @Override
         public void addBook(Book book) throws RemoteException {
             books.add(book);
-            for (IOnNewBookArrivedListener onNewBookArrivedListener : onNewBookArrivedListeners) {
-                onNewBookArrivedListener.onNewBookArrived(book);
+            final int N = onNewBookArrivedListeners.beginBroadcast();
+            for (int i = 0; i < N; i++) {
+                IOnNewBookArrivedListener onNewBookArrivedListener = onNewBookArrivedListeners.getBroadcastItem(i);
+                if (onNewBookArrivedListener != null) {
+                    try {
+                        onNewBookArrivedListener.onNewBookArrived(book);
+                    } catch (Exception e) {
+                        Log.e(TAG, e.getMessage());
+                    }
+                }
             }
-            new Thread() {
+            onNewBookArrivedListeners.finishBroadcast();
+            /*new Thread() {
                 @Override
                 public void run() {
                     SystemClock.sleep(5000);
                     throw new IllegalArgumentException("xxxx format");
                 }
-            }.start();
+            }.start();*/
         }
 
         @Override
         public void registerListener(IOnNewBookArrivedListener listener) throws RemoteException {
-            onNewBookArrivedListeners.add(listener);
+            //onNewBookArrivedListeners.add(listener);
+            onNewBookArrivedListeners.register(listener);
         }
 
         @Override
         public void unregisterListener(IOnNewBookArrivedListener listener) throws RemoteException {
-            onNewBookArrivedListeners.remove(listener);
+            //onNewBookArrivedListeners.remove(listener);
+            onNewBookArrivedListeners.unregister(listener);
         }
     }
 }

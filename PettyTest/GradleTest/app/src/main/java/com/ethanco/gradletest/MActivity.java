44717450dacafa5e0f6f621ae54f7a365caa250d
EthanCo <fenghenyu@163.com>
2017-01-05 12:58:05 +0800

    [打包] 复制apk到指定目录

--- PettyTest/GradleTest/app/src/main/java/com/ethanco/gradletest/MActivity.java
+++ PettyTest/GradleTest/app/src/main/java/com/ethanco/gradletest/MActivity.java
@@ -12,6 +12,6 @@ public class MActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_);
+        setContentView(R.layout.activity_scrolling);
     }
 }

a175ba6409daeca15111f48eb7fd3c2ac3ae787d
EthanCo <fenghenyu@163.com>
2017-01-18 16:20:47 +0800

    避免重启时提示地址被占用

--- MinaSample/MinaService/app/src/main/java/com/ethanco/mimaservice/MainActivity.java
+++ MinaSample/MinaService/app/src/main/java/com/ethanco/mimaservice/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import org.apache.mina.core.service.IoAcceptor;
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
@@ -32,7 +31,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         //创建一个IoAcceptor
-        IoAcceptor acceptor = new NioSocketAcceptor();
+        NioSocketAcceptor acceptor = new NioSocketAcceptor();
         //得到Mima为我们提供的默认的日志过滤器
         acceptor.getFilterChain().addLast("Logger", new LoggingFilter());
         //添加Codec过滤器
@@ -44,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //设置空闲时间 10后没有任何读写，回到空闲状态
         //IdleStatus.BOTH_IDLE 读和写 IdleStatus.READER_IDLE 读 IdleStatus.WRITER_IDLE 写
         acceptor.getSessionConfig().setIdleTime(IdleStatus.WRITER_IDLE, 10);
+        acceptor.setReuseAddress(true); //避免重启时提示地址被占用
         //开始监听客户端的连接 端口为9123
         try {
             acceptor.bind(new InetSocketAddress(9123));

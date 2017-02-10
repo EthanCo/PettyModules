19d3cad643c6d4bce470e20ef788a02a1c8f7c28
EthanCo <fenghenyu@163.com>
2017-02-10 10:35:47 +0800

    Lottie 吊炸天的动画，无需再手写繁琐的动画代码!

--- PettyTest2017/LottieAnimationTest/app/src/main/java/com/ethanco/lottietest/MainActivity.java
+++ PettyTest2017/LottieAnimationTest/app/src/main/java/com/ethanco/lottietest/MainActivity.java
@@ -3,6 +3,8 @@ package com.ethanco.lottietest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+//吊炸天的动画，以后无需再手写繁琐的动画!
+//https://gold.xitu.io/post/58948f1b0ce4630056f3a629
 public class MainActivity extends AppCompatActivity {
 
     @Override

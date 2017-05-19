f0d5a19e4b4c99d6afe8240812afd63d0fdbf2cf
EthanCo <fenghenyu@163.com>
2017-05-19 11:11:32 +0800

    Kotlin 单例

---
+++ PettyTest/KotlinTest/app/src/main/java/com/ethanco/kotlintest/_kotlin/Singleton.kt
@@ -0,0 +1,18 @@
+package com.ethanco.kotlintest._kotlin
+
+/**
+ * 单例
+ * 详见https://www.kotlincn.net/docs/reference/object-declarations.html
+ *
+ * @author EthanCo
+ * @since 2017/5/19
+ */
+object Singleton {
+    fun method1() {
+
+    }
+
+    fun method2() {
+
+    }
+}

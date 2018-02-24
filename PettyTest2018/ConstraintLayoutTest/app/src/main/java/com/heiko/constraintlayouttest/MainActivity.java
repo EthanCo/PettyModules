f8add6ca14adbaccc7f14873c8ce4ce29e0eee76
EthanCo <fenghenyu@163.com>
2018-02-24 14:15:24 +0800

    ConstraintLayout Test

---
+++ PettyTest2018/ConstraintLayoutTest/app/src/main/java/com/heiko/constraintlayouttest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.heiko.constraintlayouttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//https://mp.weixin.qq.com/s?src=11&timestamp=1519450192&ver=717&signature=8a77Z5Nsj-3sdfG879pERIE9Hy*7vMaOkekCn1fxNAhfz3nelGuppKV2Q9MFY3Srqpd4z2FSkcFSi6WO1p3xZXj-i9RG3chjrz6kKHIys62vQNvBy5Hr7HrE*PCaJfvs&new=1
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
